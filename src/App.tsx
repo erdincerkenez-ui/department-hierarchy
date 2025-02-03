a45db617d61f9f4da8500708c02bc084f78f0973
Erdinç Erkenez <erdincerkenez@192.168.1.59>
2025-02-03 12:35:38 +0300

    Initial commit: Added App.tsx, Department.tsx, and search.ts files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hiyerarşik Veri Arama</h1>
+      <p>organizasyon yapısı</p>
+    </div>
+  );
+};
+
+export default App;
